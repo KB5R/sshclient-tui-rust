beb1aa8ef43927e2d5461da1367fbe16169a6de9
Mr.Freeman <mihail.ku.88@gmail.com>
2025-04-15 15:49:07 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ use ssh2::Session;
 
 fn main() {
     // https://docs.rs/ssh2/latest/ssh2/ docs
-    println!("1. Подключение к SSH...");
+    println!("1. Connect to SSH...");
     let tcp = match TcpStream::connect("192.168.31.100:22"){
         Ok(stream) => {
-            println!("1.1 Подключение установлено");
+            println!("1.1 Connect successful!");
             stream  // Возвращаем поток
         }
         Err(e) => {
-            println!("1.1 Ошибка подключения {}", e);
+            println!("1.1 Connect error {}", e);
             return;
         }
     };
