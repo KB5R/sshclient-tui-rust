55ad7b8f122ddd7c61a03365fcf38230ab9d0796
Mr.Freeman <mihail.ku.88@gmail.com>
2025-04-16 18:01:29 +0300

    Add check log pass

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,44 @@ use std::io::{Read, Write};
 use std::net::TcpStream;
 use ssh2::Session;
 
-
 fn main() {
-    // https://docs.rs/ssh2/latest/ssh2/ docs
     println!("1. Connect to SSH...");
-    let tcp = match TcpStream::connect("192.168.31.100:22"){
+    let tcp = match TcpStream::connect("192.168.31.100:22") {
         Ok(stream) => {
             println!("1.1 Connect successful!");
-            stream  // Возвращаем поток
-        }
-        Err(e) => {
-            println!("1.1 Connect error {}", e);
-            return;
-        }
-    };
-
-    let mut sess = match Session::new(){
-        Ok(stream) => {
-            println!("1.2 Creat session successful! ");
             stream
         }
         Err(e) => {
-            println!("1.2 Creat session error {}", e);
+            println!("1.1 Connect error: {}", e);
             return;
         }
     };
 
+    let mut sess = match Session::new() {
+        Ok(s) => {
+            println!("1.2 Create session successful!");
+            s
+        }
+        Err(e) => {
+            println!("1.2 Create session error: {}", e);
+            return;
+        }
+    };
 
     sess.set_tcp_stream(tcp);
     sess.handshake().unwrap();
 
-    sess.userauth_password("pmk", "chiiiina").unwrap();
-    assert!(sess.authenticated());
+    match sess.userauth_password("pmk", "chiiiina") {
+        Ok(_) => println!("2. Authenticated with login & password!"),
+        Err(e) => {
+            println!("2. Auth failed: {}", e);
+            return;
+        }
+    }
+
+    if sess.authenticated() {
+        println!("2.1 Authenticated successfully!");
+    } else {
+        println!("2.1 Authentication failed!");
+    }
 }
